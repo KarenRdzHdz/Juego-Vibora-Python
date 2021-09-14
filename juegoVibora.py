deabbad3669c7a21637f3d6e9643e0c585590539
Jorge Eduardo Arias Arias <joredu.arias.42@gmail.com>
2021-09-14 10:42:31 -0500

    Update juegoVibora.py

--- juegoVibora.py
+++ juegoVibora.py
@@ -26,13 +26,16 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
+s = 100
 
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+def changeSpeed(sp):
+    global s 
+    s = sp
 
 def inside(head):
     "Return True if head inside boundaries."
@@ -43,14 +46,14 @@ def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
-
+    
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
 
     snake.append(head)
-
+    
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
@@ -65,16 +68,20 @@ def move():
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    ontimer(move, s)
 
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+onkey(lambda: changeSpeed(200), 'q')
+onkey(lambda: changeSpeed(100), 'w')
+onkey(lambda: changeSpeed(50), 'e')
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+print(snake)
 done()
