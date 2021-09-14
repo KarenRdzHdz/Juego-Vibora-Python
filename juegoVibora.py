29616d32d1dfc25860ceedb29fc4532adfae2ada
Jorge Eduardo Arias Arias <joredu.arias.42@gmail.com>
2021-09-14 11:31:24 -0500

    Update juegoVibora.py

--- juegoVibora.py
+++ juegoVibora.py
@@ -14,40 +14,45 @@ Hernán Salinas Ibarra - A01570409
 
 14/09/2021
 
-Ejercicios denotados por ***ejercicio realizado***
+Exercises marked by ***ejercicio realizado***
 
 """
-
+# Required libraries imported
 from random import randrange
 from turtle import *
 import random
 
 from freegames import square, vector
 
+# Pre-stablished variables declared
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+#   Default speed stablished at 100
 s = 100
 
-#Shuffle and set colors
-colors = ['blue','green', 'yellow', 'purple', 'black']
+#   Shuffle and set colors
+colors = ['blue','green', 'yellow', 'purple', 'black']  # ***Exercise 5: change colors***
 random.shuffle(colors)
 
-
+# Function to change snake direction
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
-def changeSpeed(sp):
+# Function to change snake speed
+def changeSpeed(sp):                                    # ***Exercise 1: changing the speed***                     
     global s 
     s = sp
 
+# Function to check snake in boundaries
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
-def move_food():
+# Function to change food location
+def move_food():                                        # ***Exercise 3: move the food***        
     "Food random without exiting boundaries"
     new_food_position_x = randrange(-1,1) * 10
     new_food_position_y = randrange(-1,1) * 10
@@ -61,7 +66,7 @@ def move_food():
     if (new_food_position_y == 10) and (food.y < 140):
         food.y = food.y + new_food_position_y
 
-
+# Main movement function
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
@@ -71,7 +76,8 @@ def move():
         square(head.x, head.y, 9, 'red')
         update()
         return
-    if not inside(head):
+    
+    if not inside(head):                                # ***Exercise 2: go around edges***        
         if head.x < -200:
             head.x = 190
         if head.x > 190:
@@ -101,7 +107,8 @@ def move():
     update()
     ontimer(move, s)
 
-def clickLeft(x, y):
+# Functions to change direction with mouse clicks
+def clickLeft(x, y):                                    # ***Exercise 4: respond to mouse clicks***
     change(-10, 0)
 
 def clickRight(x, y):
@@ -113,6 +120,8 @@ def clickMiddle(x, y):
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
+
+# Input detection
 listen()
 onkey(lambda: changeSpeed(200), 'q')
 onkey(lambda: changeSpeed(100), 'w')
@@ -126,6 +135,6 @@ onscreenclick(clickLeft, btn=1)
 onscreenclick(clickRight, btn=3)
 onscreenclick(clickMiddle, btn=2)
 
+# Execution
 move()
-print(snake)
 done()
